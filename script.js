a1db7a3f58823c4a993425837956d050143e4b20
Horquidia <horquidia.a.lima@gmail.com>
2021-08-17 13:51:25 -0300

    substituido dados html por js

--- script.js
+++ script.js
@@ -10,3 +10,66 @@ const Modal = {
             .classList.remove('active')
     }
 }
+//toggle
+
+const transactions = [
+    {
+        id: 1,
+        description: 'Luz',
+        amount: -50000, //-500
+        date: '23/01/2021'
+    }, 
+    {
+        id: 2,
+        description: 'Website',
+        amount: -5000000, //-5000
+        date: '23/01/2021'
+    },
+    {
+        id: 3,
+        description: 'Internet',
+        amount: -20000, //-200
+        date: '23/01/2021'
+    }
+]
+
+const Transaction = {
+    incomes() {
+        //somar as entradas
+    },
+    expenses() {
+        //somar as saidas
+    },
+    total() {
+        //entradas - saidas
+    }
+}
+
+const DOM = {
+    transactionsContainer : document.querySelector('#data-table tbody'),
+    addTransaction(transaction, index) {
+        const tr = document.createElement('tr')
+        tr.innerHTML = DOM.innerHTMLTransaction(transaction)
+
+        DOM.transactionsContainer.appendChild(tr)
+    },
+
+    innerHTMLTransaction(transaction) {
+        const html = `
+        
+            <td class="description">${transaction.description}</td>
+            <td class="expense">${transaction.amount}</td>
+            <td class="date">${transaction.date}</td>
+            <td>
+                <img src="./assets/minus.svg" alt="remover transacion">
+            </td>
+        
+        `
+        return html
+    }
+}
+
+
+transactions.forEach(function(transaction) {
+    DOM.addTransaction(transaction)
+})
